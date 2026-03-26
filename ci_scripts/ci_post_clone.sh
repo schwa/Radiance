32557cf2900373b98be23f0b8c832e92da3bc37c
Jonathan Wight <jwight@mac.com>
2026-03-25 20:14:14 -0700

    Unconditionally skip package plugin and macro fingerprint validation in CI post-clone script, removing the `CI_XCODE_CLOUD` guard.

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 set -e
 
-if [ "$CI_XCODE_CLOUD" = "TRUE" ]; then
-    defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES
-fi
+defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES
+defaults write com.apple.dt.Xcode IDESkipMacroFingerprintValidation -bool YES
